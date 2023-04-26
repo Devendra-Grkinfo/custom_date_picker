28f160a6716fb4edb51e294e709e36be1ea81e23
Shivanshtomar05 <shivansh.tomar.grkinfo@gmail.com>
2023-04-26 19:51:57 +0530

    Date_Time_Picker_Tool_Tip_Error_Message

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,25 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import DateTimePicker from './DateTimePicker';
 
 
 const Main = () => {
-    const [inputValue, setInputValue] = useState("");
-
-    const handleInputChange = (value) => {
-        setInputValue(value);
-        // console.log(value)
-    };
-
     return (
         <div className="main">
             <DateTimePicker
                 timeInterval={60}
                 listIntervals={5}
                 showTimer={true}
-                value={inputValue}
                 placeholder="Select a date and time"
                 selectedColor="forest green"
-                handleInputChange={handleInputChange}
             />
         </div>
     );
